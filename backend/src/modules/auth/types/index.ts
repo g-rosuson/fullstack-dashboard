f0f4cab9b12dc74c898cd0df9e9d85160ce5735c
GHR <g.rosuson@gmail.com>
2026-03-19 16:04:01 +0100

    chore(backend): add comments to type file

--- backend/src/modules/auth/types/index.ts
+++ backend/src/modules/auth/types/index.ts
@@ -2,8 +2,19 @@ import { z } from 'zod';
 
 import { loginUserPayloadSchema, registerUserPayloadSchema } from '../schemas';
 
+/**
+ * A login user payload schema.
+ */
 type LoginUserPayload = z.infer<typeof loginUserPayloadSchema>;
+
+/**
+ * A register user payload schema.
+ */
 type RegisterUserPayload = z.infer<typeof registerUserPayloadSchema>;
+
+/**
+ * A create user payload schema.
+ */
 type CreateUserPayload = Omit<RegisterUserPayload, 'confirmationPassword'>;
 
 export type { LoginUserPayload, RegisterUserPayload, CreateUserPayload };
