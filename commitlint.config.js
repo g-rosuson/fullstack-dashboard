1f373bc6883436230dfd565e2f27794b6180adef
GHR <g.rosuson@gmail.com>
2026-04-01 14:12:04 +0200

    feat(global): add prettier script and format files

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,18 +3,12 @@ module.exports = {
     parserPreset: {
         parserOpts: {
             headerPattern: /^(\w*)\((\w*)\): (.*)$/,
-            headerCorrespondence: ['type', 'scope', 'subject']
-        }
+            headerCorrespondence: ['type', 'scope', 'subject'],
+        },
     },
     rules: {
-        'type-enum': [2,
-            'always',
-            ['feat', 'fix', 'refactor', 'chore', 'test', 'docs']],
-        'scope-enum': [
-            2,
-            'always',
-            ['backend', 'frontend', 'global']
-        ],
-        'scope-empty': [2, 'never']
-    }
+        'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'chore', 'test', 'docs']],
+        'scope-enum': [2, 'always', ['backend', 'frontend', 'global']],
+        'scope-empty': [2, 'never'],
+    },
 };
--- frontend/vite.config.mts
+++ frontend/vite.config.mts
@@ -1,16 +1,16 @@
 /// <reference types="vitest" />
 
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "test/vitest.setup.ts",
-  }
-})
+    plugins: [react(), tailwindcss(), tsconfigPaths()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: 'test/vitest.setup.ts',
+    },
+});
