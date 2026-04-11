5492ded5ce919d143c0f5c661bedebdbf64fe9da
GHR <g.rosuson@gmail.com>
2026-04-11 14:07:13 +0200

    feat(frontend): add field comp

---
+++ frontend/src/components/ui-app/field/Field.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import type { FieldProps } from './Field.types';
+
+import { Field as ShadcnField, FieldLabel } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+
+/**
+ * A standard field component for the tool dialog.
+ */
+const Field: React.FC<FieldProps> = ({ label, type, placeholder, required, name, value, onChange }) => {
+    const id = `${name}-field`;
+
+    return (
+        <ShadcnField>
+            <FieldLabel htmlFor={id} className="gap-0.5">
+                {label}
+                {required && <span className="text-destructive">*</span>}
+            </FieldLabel>
+            <Input
+                name={name}
+                value={value}
+                id={id}
+                type={type}
+                placeholder={placeholder}
+                required={required}
+                onChange={onChange}
+            />
+        </ShadcnField>
+    );
+};
+
+export default Field;
