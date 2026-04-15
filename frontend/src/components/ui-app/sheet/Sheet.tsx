8d4eff98f07e71ea8673acc4a13366eb7d237e89
GHR <g.rosuson@gmail.com>
2026-04-15 19:53:14 +0200

    feat(frontend): add sheet comp

--- frontend/src/components/ui-app/sheet/Sheet.tsx
+++ frontend/src/components/ui-app/sheet/Sheet.tsx
@@ -1,33 +1,59 @@
 import React from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
 
+import type { SheetProps } from './Sheet.types';
+
+import { Button } from '@/components/ui/button';
+import { DialogFooter } from '@/components/ui/dialog';
 import { Sheet as SheetPrimitive, SheetContent } from '@/components/ui/sheet';
+import { Spinner } from '@/components/ui/spinner';
 import { cn } from '@/lib/utils';
 
-const sheetVariants = cva('p-4', {
-    variants: {
-        width: {
-            quarter: 'data-[side=right]:w-1/4 data-[side=right]:sm:max-w-none',
-            third: 'data-[side=right]:w-1/3 data-[side=right]:sm:max-w-none',
-            half: 'data-[side=right]:w-1/2 data-[side=right]:sm:max-w-none',
-            full: 'data-[side=right]:w-full data-[side=right]:sm:max-w-none',
-        },
-    },
-    defaultVariants: {
-        width: 'third',
-    },
-});
-
-type Props = VariantProps<typeof sheetVariants> &
-    React.ComponentProps<typeof SheetPrimitive> & {
-        children: React.ReactNode;
-        contentClassName?: string;
+const Sheet = ({
+    children,
+    className,
+    enableForm,
+    onFormSubmit,
+    onPrimaryButtonClick,
+    isSubmitting,
+    primaryButtonLabel,
+    ...props
+}: SheetProps) => {
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onFormSubmit?.(e);
     };
 
-const Sheet = ({ width, children, contentClassName, ...props }: Props) => {
+    const footer = (
+        <DialogFooter className="sticky bottom-0 left-0 right-0 rounded-none mt-6" showCloseButton>
+            {primaryButtonLabel && (
+                <Button
+                    type={enableForm ? 'submit' : 'button'}
+                    variant="default"
+                    disabled={isSubmitting}
+                    {...(!enableForm && { onClick: e => onPrimaryButtonClick?.(e) })}>
+                    {isSubmitting ? <Spinner /> : primaryButtonLabel}
+                </Button>
+            )}
+        </DialogFooter>
+    );
+
+    const inner = enableForm ? (
+        <form onSubmit={onSubmit} className="h-full grid grid-rows-[1fr_auto]">
+            {children}
+            {footer}
+        </form>
+    ) : (
+        <div className="h-full grid grid-rows-[1fr_auto]">
+            {children}
+            {footer}
+        </div>
+    );
+
+    const baseClassName = 'overflow-scroll p-4 pb-0';
+
     return (
         <SheetPrimitive {...props}>
-            <SheetContent className={cn(sheetVariants({ width }), contentClassName)}>{children}</SheetContent>
+            <SheetContent className={cn(baseClassName, className)}>{inner}</SheetContent>
         </SheetPrimitive>
     );
 };
