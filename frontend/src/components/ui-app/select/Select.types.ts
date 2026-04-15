ba1d305eb3decb2fb1325621e5df98f82ee008fc
GHR <g.rosuson@gmail.com>
2026-04-15 17:22:01 +0200

    feat(frontend): add select comp

--- frontend/src/components/ui-app/select/Select.types.ts
+++ frontend/src/components/ui-app/select/Select.types.ts
@@ -1,13 +1,19 @@
-type SelectOption<V extends string = string> = {
+/**
+ * The props for the Select component.
+ */
+interface SelectOption<V extends string = string> {
     value: V;
     label: string;
-};
+}
 
+/**
+ * The props for the Select component.
+ */
 interface SelectProps<V extends string> {
     label: string;
     options: ReadonlyArray<SelectOption<V>>;
     id: string;
-    value: V | '';
+    value: V | string;
     placeholder: string;
     // eslint-disable-next-line no-unused-vars
     onChange: (option: SelectOption<V> | undefined) => void;
